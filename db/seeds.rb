b5c54340b5a819bb965b1968b9ba5c2a62aff886
Fabien alexandre <alexandre.fabien@gmail.com>
2022-02-17 11:01:14 +0100

    Exercice en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+london_cosy = { name: 'Light & Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3 }
+madrid_cosy = { name: 'Light & Spacious Garden Flat madrid', address: '147 rue des lilas', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 56, number_of_guests: 2 }
+berlin_cosy = { name: 'Light & Spacious Garden Flat berlin', address: '21 boulevard shuman', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 87, number_of_guests: 4 }
+paris_cosy = { name: 'Light & Spacious Garden Flat paris', address: '17 rue des lapins', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 90, number_of_guests: 2 }
+[ london_cosy, madrid_cosy, berlin_cosy, paris_cosy ].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
